8b272fce54370c2da6d89ff893776a591ab35146
paulsans <paulsanswrk@gmail.com>
2025-09-04 15:05:44 +0600

    Task 5: Добавлены условные блоки для отключения стилей и скриптов при GET-параметре simple=1

--- templates/head.php
+++ templates/head.php
@@ -2,6 +2,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Мой Шаблон Страницы</title>
+    <?php if (!isset($_GET['simple']) || $_GET['simple'] != '1'): ?>
     <link rel="stylesheet" href="/assets/css/styles.css"> <!-- Подключи свой CSS-файл -->
     <script src="/assets/js/axnikitaJS.js"></script>
+    <?php endif; ?>
 </head>
